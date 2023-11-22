f995559dd0a55792a3c70c93942e9489491c8dc4
Davydova Olha <o.chookity@gmail.com>
2023-11-22 20:25:24 +0200

    Add models to admin page

--- post/admin.py
+++ post/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from post.models import HashTag, Post, Like, Comment
+from user.admin import WhoDidItAdmin
+
+admin.site.register(HashTag)
+admin.site.register(Like)
+
+
+@admin.register(Post)
+class PostAdmin(WhoDidItAdmin):
+    list_display = ("title", "created_by", "created_at", "updated_at")
+    search_fields = ("title", "created_by__email",)
+    ordering = ("created_by", "-updated_at",)
+
+
+@admin.register(Comment)
+class CommentAdmin(WhoDidItAdmin):
+    list_display = ("post", "content", "created_by", "created_at", "updated_at")
+    search_fields = ("created_by__email",)
+    ordering = ("post", "-updated_at",)
--- user_profile/admin.py
+++ user_profile/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from user.admin import WhoDidItAdmin
+from user_profile.models import UserProfile
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(WhoDidItAdmin):
+    list_display = ("created_by", "bio", "created_at", "updated_at")
+    search_fields = ("created_by__email",)
+    ordering = ("created_by", "-updated_at",)
