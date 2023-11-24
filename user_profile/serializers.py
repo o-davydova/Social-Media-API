6f6fe501d830190675ad0d6455998fe7d911a92d
Davydova Olha <o.chookity@gmail.com>
2023-11-24 16:03:20 +0200

    Implement `UserProfile` serializers: `UserProfileFollow`, `UserProfileImage`, `UserProfile`, `UserProfileList`, `UserProfileDetail`

---
+++ user_profile/serializers.py
@@ -0,0 +1,65 @@
+from rest_framework import serializers
+
+from user.serializers import WhoDidItSerializer
+from user_profile.models import UserProfile, UserProfileFollow
+
+
+class UserProfileFollowSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfileFollow
+        fields = ("id", "follower", "following")
+
+
+class UserProfileImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = ("id", "image")
+
+
+class UserProfileSerializer(WhoDidItSerializer, serializers.ModelSerializer):
+    followers = UserProfileFollowSerializer(many=True, read_only=True)
+    following = UserProfileFollowSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = UserProfile
+        fields = (
+            "id",
+            "bio",
+            "image",
+            "followers",
+            "following",
+        ) + WhoDidItSerializer.Meta.fields
+
+
+class UserProfileListSerializer(UserProfileSerializer):
+    followers_count = serializers.IntegerField(read_only=True)
+    following_count = serializers.IntegerField(read_only=True)
+    posts_count = serializers.IntegerField(read_only=True)
+
+    class Meta:
+        model = UserProfile
+        fields = (
+            "id",
+            "bio",
+            "image",
+            "followers_count",
+            "following_count",
+            "posts_count",
+        ) + WhoDidItSerializer.Meta.fields
+
+
+class UserProfileDetailSerializer(UserProfileSerializer):
+    followers = serializers.SlugRelatedField(
+        many=True, read_only=True, slug_field="follower_id"
+    )
+    following = serializers.SlugRelatedField(
+        many=True, read_only=True, slug_field="following_id"
+    )
+
+    class Meta:
+        model = UserProfile
+        fields = UserProfileSerializer.Meta.fields + (
+            "followers",
+            "following",
+            "posts",
+        )
