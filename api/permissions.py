1a2e054cf229355605368cb75de57fddf4693f59
Davydova Olha <o.chookity@gmail.com>
2023-11-25 18:19:57 +0200

    Implement `CanModifyOwnObjectOnly` permission

---
+++ api/permissions.py
@@ -0,0 +1,9 @@
+from rest_framework.permissions import SAFE_METHODS, BasePermission
+
+
+class CanModifyOwnObjectOnly(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return bool(
+            request.method in ('GET', 'HEAD', 'OPTIONS', 'POST')
+            or obj.created_by == request.user
+        )
