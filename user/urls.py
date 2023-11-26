738421a4e83e548c8b49539ec5d7de0eecf51ba9
Davydova Olha <o.chookity@gmail.com>
2023-11-27 00:11:22 +0200

    Implement `ManageUserView`

--- user/urls.py
+++ user/urls.py
@@ -5,13 +5,14 @@ from rest_framework_simplejwt.views import (
     TokenVerifyView,
 )
 
-from user.views import CreateUserView, LogoutView, LogoutAllView
+from user.views import CreateUserView, LogoutView, LogoutAllView, ManageUserView
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path("me/", ManageUserView.as_view(), name="manage"),
     path("logout/", LogoutView.as_view(), name="auth_logout"),
     path("logout_all/", LogoutAllView.as_view(), name="auth_logout_all"),
 ]
--- user/views.py
+++ user/views.py
@@ -1,17 +1,27 @@
 from rest_framework import generics, views, status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework_simplejwt.token_blacklist.models import (
     OutstandingToken,
     BlacklistedToken,
 )
 from rest_framework_simplejwt.tokens import RefreshToken
 
-from user.serializers import UserSerializer
+from user.serializers import UserSerializer, UserDetailSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserDetailSerializer
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
 
 
 class LogoutView(views.APIView):
