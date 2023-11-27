31488e1266813c3bb7e5ca8257330bb98e766bd1
Davydova Olha <o.chookity@gmail.com>
2023-11-27 16:40:31 +0200

    Add throttling for project

--- social_media/settings.py
+++ social_media/settings.py
@@ -138,6 +138,11 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
     ],
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {"anon": "10/minute", "user": "30/minute"},
 }
 
 SIMPLE_JWT = {
