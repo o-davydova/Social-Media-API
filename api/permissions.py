8a1007b0ab80d9594cc3999b486054a7c0c3b14a
Davydova Olha <o.chookity@gmail.com>
2023-12-04 22:17:19 +0200

    Refactor `IsOwnerOrReadOnly` permission & Add `IsOwnerOnly` permission

--- api/permissions.py
+++ api/permissions.py
@@ -1,9 +1,14 @@
 from rest_framework.permissions import BasePermission
 
 
-class CanModifyOwnObjectOnly(BasePermission):
+class IsOwnerOrReadOnly(BasePermission):
     def has_object_permission(self, request, view, obj):
         return bool(
-            request.method in ("PUT", "PATCH", "DELETE")
-            and obj.created_by == request.user
+            obj.created_by == request.user
+            or request.method in ("GET", "HEAD", "OPTIONS", "POST")
         )
+
+
+class IsOwnerOnly(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return bool(obj.created_by == request.user)
