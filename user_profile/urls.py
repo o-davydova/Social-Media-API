c7214172a214b82899ac1dace16e2aa085ed3cf2
Davydova Olha <o.chookity@gmail.com>
2023-12-05 12:55:23 +0200

    Add `UserProfileFollow` to urlpatterns and admin site

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -1,9 +1,10 @@
 from rest_framework import routers
 
-from user_profile.views import UserProfileViewSet
+from user_profile.views import UserProfileViewSet, UserProfileFollowViewSet
 
 router = routers.DefaultRouter()
 router.register("user_profiles", UserProfileViewSet)
+router.register("user_profile_follows", UserProfileFollowViewSet)
 
 urlpatterns = router.urls
 
